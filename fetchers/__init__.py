74be2e0298d0e18cfdf5b703fc99db095682d0f1
Light <lightman.chang@gmail.com>
2026-04-26 11:06:58 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,39 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .aidc import AidcFetcher
+from .bae import BaeFetcher
+from .elbit import ElbitFetcher
+from .general_dynamics import GeneralDynamicsFetcher
+from .hanwha_aero import HanwhaAeroFetcher
+from .kratos import KratosFetcher
+from .l3harris import L3harrisFetcher
+from .leonardo import LeonardoFetcher
+from .lockheed import LockheedFetcher
+from .northrop import NorthropFetcher
+from .palantir import PalantirFetcher
+from .rheinmetall import RheinmetallFetcher
+from .rtx import RtxFetcher
+from .saab import SaabFetcher
+from .thales import ThalesFetcher
+
+FETCHERS = {
+    "aidc": AidcFetcher,
+    "bae": BaeFetcher,
+    "elbit": ElbitFetcher,
+    "general_dynamics": GeneralDynamicsFetcher,
+    "hanwha_aero": HanwhaAeroFetcher,
+    "kratos": KratosFetcher,
+    "l3harris": L3harrisFetcher,
+    "leonardo": LeonardoFetcher,
+    "lockheed": LockheedFetcher,
+    "northrop": NorthropFetcher,
+    "palantir": PalantirFetcher,
+    "rheinmetall": RheinmetallFetcher,
+    "rtx": RtxFetcher,
+    "saab": SaabFetcher,
+    "thales": ThalesFetcher,
+}
